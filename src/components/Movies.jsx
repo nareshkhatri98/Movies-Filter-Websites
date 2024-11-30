9429a7f2cc75f9d1e2422976d0a01f3377a5137e
Naresh Khatri <nareshkhatri9810@gmail.com>
2024-11-30 11:42:50 +0545

    Fetch the data form API

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -4,17 +4,19 @@ import { NavLink } from 'react-router-dom';
 
 const Movies = () => {
   const { movie } = useGlobalContext();
+  
   return (
     <>
       <section className='movie-page'>
-        <div className='grid grid-4-col'>
+        <div className='container grid grid-4-col'>
           {movie.map((currMovie) => {
             const { imdbID, Title, Poster } = currMovie;
+            const movieName = Title.substring(0,15)
             return (
               <NavLink to={`/movie/${imdbID}`} key={imdbID}>
                 <div className='card'>
                   <div className='card-info'>
-                    <h2>{Title}</h2>
+                    <h2>{movieName.length>=15 ? `${movieName} ...` : movieName}</h2>
                     <img src={Poster} alt={Title} />
                   </div>
                 </div>
--- src/context/context.jsx
+++ src/context/context.jsx
@@ -6,52 +6,44 @@
 // create context hooks.
 import React, { useContext, useEffect, useState } from "react";
 
-const API_URL =` http://www.omdbapi.com/?i=tt3896198&apikey=f75fc6b&s=titanic`
+const API_URL = `http://www.omdbapi.com/?i=tt3896198&apikey=${import.meta.env.VITE_API_KEY}&s=titanic`;
 
 const AppContext = React.createContext();
-// we need to create provider function..
 
-const AppProvider = ({children}) =>{
-
+const AppProvider = ({ children }) => {
   const [isLoading, setIsLoading] = useState(true);
   const [movie, setMovie] = useState([]);
-  const [isError,setIsError] = useState({show:"false", message: ""});
+  const [isError, setIsError] = useState({ show: false, message: "" });
 
-  const getMovies = async(url) =>{
-    try{
-      const res = await fetch(url)
+  const getMovies = async (url) => {
+    try {
+      const res = await fetch(url);
       const data = await res.json();
-      console.log(data);
 
-      if(data.Response === "True")
-      {
-         setIsLoading(false);
-         setMovie(data.Search);
+      if (data.Response === "True") {
+        setIsLoading(false);
+        setMovie(data.Search);
+      } else {
+        setIsError({ show: true, message: data.Error });
       }
-      else{
-        setIsError({
-          show:"true",
-          message:data.error,
-        })
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-      }
-    }catch(err)
-    {console.log(err)}
+  useEffect(() => {
+    getMovies(API_URL);
+  }, []);
 
-  }
+  return (
+    <AppContext.Provider value={{ isLoading, isError, movie }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
 
-  useEffect(()=>{
-    getMovies(API_URL); 
-  },[])
-  return <AppContext.Provider value={{isLoading, isError,movie}}>
-    {children}
-  </AppContext.Provider>
-} 
-
-// Global custom hook
-const useGlobalContext =()=>{
+const useGlobalContext = () => {
   return useContext(AppContext);
-}
+};
 
-
-export {AppContext,AppProvider, useGlobalContext}
+export { AppContext, AppProvider, useGlobalContext };
